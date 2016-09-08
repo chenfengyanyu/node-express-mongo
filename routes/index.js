ab2bde58b423eceafd43eafce70aa00e912bec86
chenfengyanyu <jartto@qq.com>
2016-09-08 23:30:34 +0800

    jartto:complete basic facility

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,48 @@
 var express = require('express');
+// var app = express();
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+var User = require('../db/db');
+
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var temps = [{
-  		'name':'jartto',
-  		'phone':'18612345678'
-	  },{
-  		'name':'test',
-  		'phone':'12345678910'
-	  }]
-  res.render('index', { args : temps});
+	User.find({},function(err,datas){
+		 if (err) {
+		    console.log(err);
+		  } else {
+		    res.render('index', { args : datas});
+		  }
+	})
+});
+
+router.post('/add', urlencodedParser , function(req, res, next) {
+	var theOne = new User({
+		name:req.body.name,
+		phone:req.body.phone
+	});
+	theOne.save(function (err) {
+	  if (err) {
+	    console.log(err);
+	  } else {
+	    console.log('add success!');
+	    res.redirect('/');
+	  }
+	});
+});
+
+router.get('/delete', function(req, res, next) {
+	User.remove({_id:req.query.id},function (err) {
+	  if (err) {
+	    console.log(err);
+	  } else {
+	    console.log('delete success!');
+	    res.redirect('/');
+	  }
+	});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../db/db');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  	User.findOne({_id:req.query.id},function(err,data){
+		if (err) {
+		    console.log(err);
+		  } else {
+		  	// console.log(data);
+		    res.render('users', { args : data});
+		  }
+	})
+	
 });
 
 router.get('/name', function(req, res, next) {
