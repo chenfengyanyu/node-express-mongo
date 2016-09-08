7a2e58a79ca097b3406be776f76b6c6b08475a9b
chenfengyanyu <jartto@qq.com>
2016-09-08 12:06:32 +0800

    jartto:complete render data

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 a demo for express and mongodb
 
 ## 安装
+clone项目后，请先安装npm包依赖：
 ```sh
 npm install
 ```
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var temps = [{
+  		'name':'jartto',
+  		'phone':'18612345678'
+	  },{
+  		'name':'test',
+  		'phone':'12345678910'
+	  }]
+  res.render('index', { args : temps});
 });
 
 module.exports = router;
